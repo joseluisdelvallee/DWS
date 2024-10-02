293b19b5a5d0f93820760da3eefff92a2a94d72c
joseluisdelvallee <j.l.del.valle.del.pino@gmail.com>
2024-10-02 11:58:49 +0200

    POST a GET

--- ejerciciosIniciales/formularios/recoger.php
+++ ejerciciosIniciales/formularios/recoger.php
@@ -1,42 +1,42 @@
 <?php
 
-    //print_r($_POST);
+    //print_r($_GET);
     //echo '<br />';
 
     /*
-    foreach ($_POST as $indice => $valor) {
+    foreach ($_GET as $indice => $valor) {
         echo $indice.': '.$valor.'<br>';
     }
     */
 
     // Recoger datos a variables
-    if(!empty($_POST['nombre'])) {
-        echo 'Nombre: '.$_POST['nombre'].'<br />';
+    if(!empty($_GET['nombre'])) {
+        echo 'Nombre: '.$_GET['nombre'].'<br />';
     }
 
-    if(!empty($_POST['apellidos'])) {
-        echo 'Apellidos: '.$_POST['apellidos'].'<br />';
+    if(!empty($_GET['apellidos'])) {
+        echo 'Apellidos: '.$_GET['apellidos'].'<br />';
     }
 
-    if(isset($_POST['sexo'])) {
-        echo 'Sexo: '.$_POST['sexo'].'<br />';
+    if(isset($_GET['sexo'])) {
+        echo 'Sexo: '.$_GET['sexo'].'<br />';
     }
     
-    if(!empty($_POST['nomDesastre'])) {
-        echo 'Nombre Desastre: '.$_POST['nomDesastre'].'<br />';
+    if(!empty($_GET['nomDesastre'])) {
+        echo 'Nombre Desastre: '.$_GET['nomDesastre'].'<br />';
     }
 
-    if(isset($_POST['tipo'])){
-        echo 'Tipo: '.$_POST['tipo'].'<br />';
+    if(isset($_GET['tipo'])){
+        echo 'Tipo: '.$_GET['tipo'].'<br />';
     }
 
-    if(!empty($_POST['descripcion'])) {
-        echo 'Descripcion: '.$_POST['descripcion'].'<br />';
+    if(!empty($_GET['descripcion'])) {
+        echo 'Descripcion: '.$_GET['descripcion'].'<br />';
     }
 
-    if(isset($_POST['notificaciones'])) {
+    if(isset($_GET['notificaciones'])) {
         echo 'Notificaciones: ';
-        foreach ($_POST['notificaciones'] as $notif) {
+        foreach ($_GET['notificaciones'] as $notif) {
             echo $notif.' ';
         }
         echo '<br />';
